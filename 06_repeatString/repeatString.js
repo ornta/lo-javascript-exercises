9d6656f4c190466331bf100dd27498e83cc28290
ornta <47490446+ornta@users.noreply.github.com>
2025-11-10 07:33:40 -0800

    update solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    if (num === 0) return "";
+    if (num < 0) return "ERROR";
+    let result = string;
+    let count = 1;
+
+    while (count < num) {
+        result += string;
+        count++;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
