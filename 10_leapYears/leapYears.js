ce8a1af264d76a52a3e079a3e75fe7b3e16e4a8f
ornta <47490446+ornta@users.noreply.github.com>
2026-01-09 06:35:41 -0800

    update solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+  if (num % 400 === 0) return true;
+  if (num % 100 === 0) return false;
+  return num % 4 === 0;
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
